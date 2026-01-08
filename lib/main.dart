cd7bd8fe2a50b947e733f0951e1660b570ccd665
wanwisa <wanwisa010448@gmail.com>
2026-01-08 11:25:52 +0700

    widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,45 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(      
       home: Scaffold(
+          appBar: AppBar(
+          title: const Text('Basic Widgets'),
+          backgroundColor: Colors.indigo, 
+          foregroundColor: Colors.white,  
+        ),
+
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // กึ่งกลางแนว
+            children: [
+            Image.asset(
+                  'assets/images/ass1.gif', 
+                  width: 150,                   
+                  fit: BoxFit.contain, 
+                ),
+              
+              const SizedBox(height: 20), // เว้นระยะห่างนิดหน่อย
+              
+              const Text(
+                'Hello World!',
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+
+        
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+          
+            debugPrint('Button Clicked');
+          },
+          backgroundColor: Colors.orange, // สีปุ่ม
+          child: const Text(
+            'Click Me',
+          
+          ),
         ),
       ),
     );
